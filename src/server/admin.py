95af2a07aabbea01a21594ccd797c080828dfad8
doossee <84768497+doossee@users.noreply.github.com>
2024-06-14 19:03:03 +0500

    it will continued

---
+++ src/server/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+
+from .models import Category, Server, Channel
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Server)
+class ServerAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Channel)
+class ChannelAdmin(admin.ModelAdmin):
+    pass
